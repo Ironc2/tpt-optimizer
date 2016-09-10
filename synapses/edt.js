89c228f430b7a1782133dbe584428e9f9125ccde
Paul Vinchon <ipvron@yahoo.fr>
2016-09-10 20:24:08 +0200

    fixes for smaller screens

--- synapses/edt.js
+++ synapses/edt.js
@@ -1,12 +1,14 @@
 var wrapper = document.getElementById("content-wrapper");
 wrapper.className = "col-md-12";
 wrapper.firstElementChild.firstElementChild.className = "col-md-12";
+wrapper.style.marginRight = "-15px";
 
 function resize() {
 	var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 	var e = document.getElementsByClassName("fc-scroller")[0];
 	if(e)
 		document.getElementsByClassName("fc-scroller")[0].style.height = (h - 300) + "px";
+	var f = document.getElementsByClassName("closed")[0];
 	setTimeout(resize, 100);
 }
 
